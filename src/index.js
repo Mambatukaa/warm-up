9556ecc72f3da28985bedc33c15fb81b7ab8808e
mambatukaa <sbatuka9@gmail.com>
2023-09-02 19:52:13 -0700

    add pipes and some refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config = {
   type: Phaser.AUTO,
@@ -8,8 +7,7 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      debug: true,
-      gravity: { y: 400 }
+      debug: true
     }
   },
   scene: {
@@ -21,32 +19,96 @@ const config = {
 
 new Phaser.Game(config);
 
-function preload(){
+function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
-const VELOCITY = 200;
-
+const VELOCITY = 400;
+const RENDER_TO_PIPE = 4;
 const flapVelocity = 300;
+
 let bird = null;
+let pipes = null;
+
+const initialBirdPosition = { x: config.width * 0.1, y: config.height * 0.5 };
+const pipeVeritcalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [300, 400];
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0);
 
-  this.input.on('pointerdown', flat)
+  bird = this.physics.add
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird')
+    .setOrigin(0);
 
-  this.input.keyboard.on('keydown_SPACE', () => {
+  bird.body.gravity.y = VELOCITY;
+  pipes = this.physics.add.group();
 
-    console.log('space')
-  })
+  for(let i = 0; i < RENDER_TO_PIPE; i++)  {
+    const upperPipe = pipes.create(0, 0, 'pipe')
+      .setOrigin(0, 1);
+
+    const lowerPipe = pipes.create(0, 0, 'pipe')
+      .setOrigin(0, 0);
+
+
+    placePipe(upperPipe, lowerPipe);
+  }
+
+  pipes.setVelocityX(-200);
+
+  this.input.on('pointerdown', flat);
+
+  this.input.keyboard.on('keydown_SPACE', () => {});
 }
 
 function update() {
-  if(bird.y < -bird.height || bird.y > config.height) {
-    alert('Game Over')
+  if (bird.y < -bird.height || bird.y > config.height) {
+    console.log('game over');
   }
+
+  recyclePipe();
+}
+
+function placePipe(uPipe, lPipe) {
+  const rightMostX = getRightMostPipe();
+
+  const pipeVerticalDistance = Phaser.Math.Between(...pipeVeritcalDistanceRange);
+  const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+
+  const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange) + rightMostX;
+
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x; 
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function(pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+
+  return rightMostX;
+}
+
+function recyclePipe() {
+  const tempPipes = [];
+  pipes.getChildren().forEach(function(pipe) {
+    if(pipe.getBounds().right < 0) {
+      tempPipes.push(pipe);
+      if(tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  })
+
 }
 
 function flat() {
